01513e6694658d82d8d18c95b3d62cc6c3227a88
Holnaider Leon <lholnaider18.lh@gmail.com>
2019-12-18 15:34:31 +0100

    Database

---
+++ src/main/java/com/devicedetection/dao/GeraetRepository.java
@@ -0,0 +1,11 @@
+package com.devicedetection.dao;
+
+import com.devicedetection.model.Geraet;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface GeraetRepository extends CrudRepository<Geraet, Integer> {
+
+}
---
+++ src/main/java/com/devicedetection/model/EventTyp.java
@@ -0,0 +1,64 @@
+package com.devicedetection.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class EventTyp {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String beschreibung;
+
+    private Integer raumNr;
+
+    private Date datumErstellung;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public Integer getRaumNr() {
+        return raumNr;
+    }
+
+    public void setRaumNr(Integer raumNr) {
+        this.raumNr = raumNr;
+    }
+
+    public Date getDatumErstellung() {
+        return datumErstellung;
+    }
+
+    public void setDatumErstellung(Date datumErstellung) {
+        this.datumErstellung = datumErstellung;
+    }
+
+
+}
